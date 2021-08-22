524b585e9133fdd2813e7f15bd4b08f76743e1e0
monkjunior <vungocson998@gmail.com>
2021-08-22 20:07:09 +0700

    Created flags and print function to structure output

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 
+	coreV1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+var (
+	label     = ""
+	field     = ""
+	namespace = ""
+)
+
 func main() {
+	flag.StringVar(&label, "labels", "", "pvc labels")
+	flag.StringVar(&field, "fields", "", "pvc fields")
+	flag.StringVar(&namespace, "namespace", "", "namespace")
+	flag.Parse()
+
 	kubeConfigFile := filepath.Join(os.Getenv("HOME"), ".kube", "config")
 
 	// Here we intend to run our cli outside of the K8S cluster (we will run it from our local terminal)
@@ -34,12 +48,20 @@ func main() {
 	api := clientSet.CoreV1()
 
 	listOptions := v1.ListOptions{
-		LabelSelector: "",
-		FieldSelector: "",
+		LabelSelector: label,
+		FieldSelector: field,
 	}
-	pvcs, err := api.PersistentVolumeClaims("monitoring").List(context.Background(), listOptions)
+	pvcs, err := api.PersistentVolumeClaims(namespace).List(context.Background(), listOptions)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.Println(pvcs.Items)
+	printPVCs(pvcs)
+}
+
+func printPVCs(pvcs *coreV1.PersistentVolumeClaimList) {
+	template := "%-96v%-8v%-8v\n"
+	fmt.Printf(template, "NAME", "STATUS", "CAPACITY")
+	for _, pvc := range pvcs.Items {
+		fmt.Printf(template, pvc.Name, string(pvc.Status.Phase), pvc.Status.Capacity.Storage().String())
+	}
 }
